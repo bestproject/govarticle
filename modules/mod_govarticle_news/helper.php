6e4d296bd0d4d74ffa8d12353070b7e643ee768e
Artur Stępień <artur.stepien@bestproject.pl>
2017-10-23 00:23:38 +0200

    Fixing documentation for mod_govarticle_news

--- modules/mod_govarticle_news/helper.php
+++ modules/mod_govarticle_news/helper.php
@@ -4,8 +4,8 @@
  * @package     GovArticle
  * @subpackage  mod_govarticle_news
  * @author      Artur Stępień (artur.stepien@bestproject.pl)
- * @copyright   (C) 2005 - 2015 Open Source Matters. All rights reserved.
- * @copyright   (C) 2015 - Fundacja PCJ Otwarte Źródła
+ * @copyright   (C) 2005 - 2017 Open Source Matters. All rights reserved.
+ * @copyright   (C) 2017 - Fundacja PCJ Otwarte Źródła
  * @license     http://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
  * @link        http://dostepny.joomla.pl
  *
